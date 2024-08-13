43a171ba529b04122b6a7646e014f81e3804df5f
sankik82 <sankik.suman@vmatechlabs.com>
2024-08-13 11:18:59 +0530

    created ramdom password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const [state,setState] = useState(" ");
+
+  const generatorPassword = ()=>{
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+
+    for(let index = 1;index <=8; index++){
+         let char = Math.floor(Math.random() * string.length);
+        setState (pass = pass + string.charAt(char));
+
+    }
+  
+  }
+
+  const cpyFunc = ()=>{
+    navigator.clipboard.writeText(state)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Randome Password Generator App</h2>
+      <input type="text" readOnly disabled name="" placeholder='Password' id=""  value={state}/>
+      <div className="btns">
+        <button onClick={generatorPassword}>Generat Password</button>
+        <button onClick={cpyFunc}>Copy Password</button>
+      </div>
     </div>
   );
 }
